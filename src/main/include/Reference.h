1d0b1d973ed08738d5d4621e07ac315c3bc71d55
2826WaveRobotics <2826WaveRobotics@users.noreply.github.com>
2020-01-30 19:39:25 -0600

    Initial commit

---
+++ src/main/include/Reference.h
@@ -0,0 +1,13 @@
+
+//CAN ID's
+#define TURRET          0
+#define SHOOTER_1       1
+#define SHOOTER_2       2
+#define FEEDER          3
+#define DRIVE_LEFT_1    4
+#define DRIVE_LEFT_2    5
+#define DRIVE_RIGHT_1   6
+#define DRIVE_RIGHT_2   7
+#define COLOR_WHEEL     8
+
+
